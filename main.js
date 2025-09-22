e609507b2911495f813cf7d66c4b430fe960ffd2
Embil22 <96885540+Embil22@users.noreply.github.com>
2025-09-22 11:20:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
+const populate = async (value, currency) => {
+    let myStr = ""
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_7UStkUqQNBmahSoy8K635tE3Sjr5fK1UVPmVloZ2&base_currency=" + currency
+    let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display = "block"
 
-const optionsList = document.querySelectorAll(".option");
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+                    </tr> 
+                `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
 
-selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
-});
-
-optionsList.forEach(o => {
-  o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
-  });
-});
+}
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='currency']").value
+    populate(value, currency)
+})
